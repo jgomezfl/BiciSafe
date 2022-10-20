3c1c5d5d8b9dc902f9174ac3ed500e73878b58a7
scasasr <64574047+scasasr@users.noreply.github.com>
2022-10-19 22:54:43 -0500

    Lugar Controller BackEnd

---
+++ BackEndComplete/demo/src/main/java/com/bicisafe/demo/web/controller/LugarController.java
@@ -0,0 +1,78 @@
+package com.bicisafe.demo.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bicisafe.demo.model.Lugar;
+import com.bicisafe.demo.service.LugarService;
+import com.bicisafe.demo.web.dto.LugarDTO;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/lugares")
+public class LugarController {
+    
+    private final LugarService lrService;
+
+    @Autowired
+    public LugarController(LugarService lrService){
+        this.lrService = lrService;
+    }
+
+    @GetMapping("/select/{id}")
+    public ResponseEntity<Lugar> getLugar(@PathVariable("id") Long id){
+        Lugar lr = lrService.getLugar(id);
+        if(lr == null){return ResponseEntity.notFound().build();}
+        return  ResponseEntity.ok(lr);
+    }
+    
+    @GetMapping("/select/all")
+    public ResponseEntity<List<Lugar>> getAll(){
+        List<Lugar> lrList = lrService.findAll();
+        if(lrList.isEmpty()){return ResponseEntity.notFound().build();}
+        return ResponseEntity.ok(lrList);
+    }
+
+    @PostMapping("/save")
+    public String createLugar(@RequestBody LugarDTO lrDto){
+        Lugar lr = new Lugar();
+        lr.setIdent(lrDto.getIdent());
+        lr.setLatitud(lrDto.getLatitud());
+        lr.setLongitud(lrDto.getLongitud());
+        lr.setTipo(lrDto.getTipo());
+
+        lrService.createLugar(lr);
+        return "Succesfull";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Lugar> deleteLugar(@PathVariable("id") Long id){
+        Lugar lr = lrService.getLugar(id);
+        if(lr == null){return ResponseEntity.notFound().build();}
+        lrService.deleteLugar(id);
+        return ResponseEntity.ok(lr);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Lugar> updateLugar(@RequestBody LugarDTO lrDto){
+        Lugar lr = lrService.getLugar(lrDto.getId());
+        if(lr  == null){ResponseEntity.notFound().build();}
+        lr = new Lugar();
+        if(lrDto.getTipo() != null){lr.setTipo(lrDto.getTipo());}
+        if(lrDto.getNombre() != null){lr.setNombre(lrDto.getNombre());}
+
+        return ResponseEntity.ok(lr);
+    }
+
+}
